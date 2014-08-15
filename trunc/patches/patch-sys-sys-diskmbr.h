af47a9e185316cabb57bf4c013ecce751703eaa2
alex.deiter@gmail.com <alex.deiter@gmail.com@8ac1cded-c1d6-ebba-ae3b-5f1a853f4175>
2014-08-15 09:48:35 +0000

    Update patches up to FreeBSD Revision 270011
    M    trunc/conf/amd64/BLACKBIRD
    M    trunc/conf/src.conf
    M    trunc/patches/patch-cddl-lib-libzpool-Makefile
    M    trunc/patches/patch-cddl-usr.bin-ztest-Makefile
    M    trunc/patches/patch-cddl-usr.sbin-zdb-Makefile
    M    trunc/patches/patch-cddl-usr.sbin-zhack-Makefile
    D    trunc/patches/patch-lib-libkse-Makefile
    M    trunc/patches/patch-lib-libthr-Makefile
    M    trunc/patches/patch-sys-boot-common-part.c
    M    trunc/patches/patch-sys-conf-files.i386
    M    trunc/patches/patch-sys-dev-viatemp-viatemp.c
    M    trunc/patches/patch-sys-dev-wbwd-wbwd.c
    M    trunc/patches/patch-sys-geom-part-g_part.c
    M    trunc/patches/patch-sys-geom-part-g_part_gpt.c
    M    trunc/patches/patch-sys-i386-conf-NOTES
    M    trunc/patches/patch-sys-modules-Makefile
    M    trunc/patches/patch-sys-modules-viatemp-Makefile
    M    trunc/patches/patch-sys-sys-diskmbr.h
    M    trunc/patches/patch-usr.sbin-Makefile
    M    trunc/patches/patch-usr.sbin-wpa-hostapd-Makefile
    M    trunc/scripts/build.sh

--- trunc/patches/patch-sys-sys-diskmbr.h
+++ trunc/patches/patch-sys-sys-diskmbr.h
@@ -1,5 +1,5 @@
---- sys/sys/diskmbr.h.orig	2014-02-26 20:12:46.000000000 +0400
-+++ sys/sys/diskmbr.h	2014-02-26 20:12:58.000000000 +0400
+--- sys/sys/diskmbr.h.orig	2014-08-15 02:23:59.000000000 +0400
++++ sys/sys/diskmbr.h	2014-08-15 02:24:11.000000000 +0400
 @@ -57,6 +57,7 @@
  #define	DOSPTYP_LINUX	0x83	/* Linux partition */
  #define	DOSPTYP_LINLVM	0x8e	/* Linux LVM partition */
