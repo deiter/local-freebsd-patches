f0fc660f91fb80ae4ca249ab1dc11f31d0266730
alex.deiter <alex.deiter@gmail.com>
2014-10-01 15:03:55 +0000

    Add quirks for 4K disks:
    INTEL SSDSC2CW120A3 400i
    ST4000VN000-1H4168 SC43
    
    A    trunc/patches/patch-sys-cam-ata-ata_da.c
    A    trunc/patches/patch-sys-cam-scsi-scsi_da.c

---
+++ trunc/patches/patch-sys-cam-ata-ata_da.c
@@ -0,0 +1,29 @@
+--- sys/cam/ata/ata_da.c.orig	2014-10-01 04:30:03.000000000 +0400
++++ sys/cam/ata/ata_da.c	2014-10-01 18:40:20.000000000 +0400
+@@ -178,6 +178,11 @@
+ 		/*quirks*/ADA_Q_4K
+ 	},
+ 	{
++		/* Seagate NAS HDD Advanced Format (4k) drives */
++		{ T_DIRECT, SIP_MEDIA_FIXED, "*", "ST????VN*", "*" },
++		/*quirks*/ADA_Q_4K
++	},
++	{
+ 		/* Seagate Barracuda Green Advanced Format (4k) drives */
+ 		{ T_DIRECT, SIP_MEDIA_FIXED, "*", "ST????DL*", "*" },
+ 		/*quirks*/ADA_Q_4K
+@@ -355,6 +360,14 @@
+ 	},
+ 	{
+ 		/*
++		 * Intel 520 Series SSDs
++		 * 4k optimised & trim only works in 4k requests + 4k aligned
++		 */
++		{ T_DIRECT, SIP_MEDIA_FIXED, "*", "INTEL SSDSC2CW*", "*" },
++		/*quirks*/ADA_Q_4K
++	},
++	{
++		/*
+ 		 * Intel X25-M Series SSDs
+ 		 * 4k optimised & trim only works in 4k requests + 4k aligned
+ 		 */
